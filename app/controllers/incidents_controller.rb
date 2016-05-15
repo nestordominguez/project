91c0a1285c5ac5452da784fd06ef4bc77d5179a2
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2016-05-15 17:54:19 -0300

    comment has been deleted from incidents

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -85,6 +85,6 @@ class IncidentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def incident_params
-      params.require(:incident).permit(:comment, :priority, :title, :content)
+      params.require(:incident).permit(:priority, :title, :content)
     end
 end

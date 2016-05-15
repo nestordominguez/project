f59a5fc7d68f13c975cde3052e7eff31d458850f
Nestor Oscar Dominguez Diaz <oscardom80@hotmail.com>
2016-05-15 16:25:01 -0300

    created association between user and company

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :incidents
+  belongs_to :company
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
